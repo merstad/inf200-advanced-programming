ef8c056bc05ad7a7ae111496779f552bda408527
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2021-10-25 15:55:32 +0200

    Implemented player class.

---
+++ lectures/l07/chutes_classes.py
@@ -0,0 +1,50 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def adjust_position(position):
+    chutes_and_ladders = {1: 12,
+                          13: 22,
+                          14: 3,
+                          20: 8}
+    if position in chutes_and_ladders:
+        return chutes_and_ladders[position]
+    else:
+        return position
+
+
+def goal_reached(position):
+    return position >= 25
+
+
+class Player:
+    def __init__(self):
+        self.position = 0
+        self.steps = 0
+
+    def move(self):
+        self.position += random.randint(1, 6)
+        self.position = adjust_position(self.position)
+        self.steps += 1
+
+
+def one_game():
+    player = Player()
+    while not goal_reached(player.position):
+        player.move()
+    return player.steps
+
+
+if __name__ == "__main__":
+    random.seed(12345678)
+    results = [one_game() for _ in range(1000)]
+
+
+    print(f'Shortest game duration: {min(results):4d}')
+    print(f'Mean game duration    : {np.mean(results):6.1f} ± {np.std(results):.1f}')
+    print(f'Longest game duration : {max(results):4d}')
+
+    hv, hb = np.histogram(results, bins=np.arange(0, max(results)))
+    plt.step(hb[:-1], hv)
+    plt.show()
