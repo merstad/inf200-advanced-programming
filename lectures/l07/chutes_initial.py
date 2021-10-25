e25d1be2d8dd46c9608c014d5b70dd872c2dc43f
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2021-10-25 15:45:56 +0200

    Improved variable names and added seeding.

--- lectures/l07/chutes_initial.py
+++ lectures/l07/chutes_initial.py
@@ -2,28 +2,29 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-res = []
+random.seed(12345678)
+results = []
 
 for _ in range(1000):
-    s = 0
-    p = 0
-    while p < 25:
-        p += random.randint(1, 6)
-        if p == 1:
-            p = 12
-        elif p == 13:
-            p = 22
-        elif p == 14:
-            p = 3
-        elif p == 20:
-            p = 8
-        s += 1
-    res.append(s)
+    steps = 0
+    position = 0
+    while position < 25:
+        position += random.randint(1, 6)
+        if position == 1:
+            position = 12
+        elif position == 13:
+            position = 22
+        elif position == 14:
+            position = 3
+        elif position == 20:
+            position = 8
+        steps += 1
+    results.append(steps)
 
-print(f'Shortest game duration: {min(res):4d}')
-print(f'Mean game duration    : {np.mean(res):6.1f} ± {np.std(res):.1f}')
-print(f'Longest game duration : {max(res):4d}')
+print(f'Shortest game duration: {min(results):4d}')
+print(f'Mean game duration    : {np.mean(results):6.1f} ± {np.std(results):.1f}')
+print(f'Longest game duration : {max(results):4d}')
 
-hv, hb = np.histogram(res, bins=np.arange(0, max(res)))
+hv, hb = np.histogram(results, bins=np.arange(0, max(results)))
 plt.step(hb[:-1], hv)
 plt.show()
