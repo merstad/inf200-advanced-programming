d541ed4ee368ccc7bef408f9f2a59051d695084b
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2022-01-13 14:40:39 +0100

    Added two-dim parameterized test function.

--- january_block/examples/test_param_example/test_demo.py
+++ january_block/examples/test_param_example/test_demo.py
@@ -37,3 +37,23 @@ def test_step_correct_increase(class_to_test, expected_step):
     x_orig = obj.x
     obj.step()
     assert obj.x == x_orig + expected_step
+
+
+@pytest.mark.parametrize('class_to_test', [B, C])
+@pytest.mark.parametrize('num_steps', [0, 1, 10, 50])
+def test_multiple_steps(class_to_test, num_steps):
+    """
+    Test that repeated calls to step() work correctly.
+
+    This test illustrates how parameterization along two dimensions can
+    be combined.
+
+    :param class_to_test: Class for which the test is to be performed
+    :param num_steps: Number of times to call step()
+    """
+    obj = class_to_test()
+    x_orig = obj.x
+    step_size = obj.s
+    for _ in range(num_steps):
+        obj.step()
+    assert obj.x == x_orig + num_steps * step_size
