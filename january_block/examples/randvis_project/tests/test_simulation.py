e11e948d0f30b2b7be25850443877b88545c592d
Hans Ekkehard Plesser <hans.ekkehard.plesser@nmbu.no>
2022-01-18 21:29:01 +0100

    Minor code changes from lecture

---
+++ january_block/examples/randvis_project/tests/test_simulation.py
@@ -0,0 +1,8 @@
+from randvis.simulation import DVSim
+
+# Should add fixture to remove generated image and movie files.
+
+def test_sim():
+    sim = DVSim((3, 5), 0.1, 12345, '.')
+    sim.simulate(5, 1, 5)
+    sim.make_movie()
